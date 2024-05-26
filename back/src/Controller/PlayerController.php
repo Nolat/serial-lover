b3daf73c4875368ca56d91a4694a2af38aa105a6
Baptiste Magliocca <baptiste.mag@live.fr>
2024-05-26 17:52:31 +0200

    ✨ Doc api
    Ajout de la documentation api

--- back/src/Controller/PlayerController.php
+++ back/src/Controller/PlayerController.php
@@ -7,13 +7,24 @@ use App\Entity\Player;
 use App\Entity\PlayerQuest;
 use App\Service\PlayerService;
 use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api/players', name: 'api_players_')]
 class PlayerController extends AbstractApiController
 {
+
+    #[OA\Response(
+        response: 200,
+        description: "Retourne l'entièreté des joueurs",
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: Player::class, groups: ['player_get']))
+        )
+    )]
+    #[OA\Tag(name: 'players',description: "API Players")]
     #[Route('', name: 'get_all', methods: 'GET')]
     public function getAllPlayers(PlayerService $playerService, Request $request) {
         $datas = [
@@ -26,6 +37,21 @@ class PlayerController extends AbstractApiController
         return $this->renderSerializeJson([]);
     }
 
+    #[OA\Response(
+        response: 200,
+        description: "Retourne le joueur modifié",
+        content: new Model(type: Player::class, groups: ['player_get'])
+    )]
+    #[OA\RequestBody(
+        description: "Envoi l'information si le joueur joue ou non",
+        required: true,
+        content: [new OA\MediaType(mediaType: "multipart/form-data",
+            schema: new OA\Schema(
+                properties: [new OA\Property(property: "is_playing", type: "bool")]
+            )
+        )]
+    )]
+    #[OA\Tag(name: 'players',description: "API Players")]
     #[Route('/{id}', name: 'update_player', methods: 'PUT')]
     public function updatePlayer(PlayerService $playerService, Request $request, $id) {
         $datas = json_decode($request->getContent(), true);
@@ -40,6 +66,12 @@ class PlayerController extends AbstractApiController
         return new JsonResponse('Une erreur c\'est produite',500, [], true);
     }
 
+    #[OA\Response(
+        response: 200,
+        description: "Retourne la quête en cours du joueur",
+        content: new Model(type: PlayerQuest::class, groups: ['player_get', 'player_quest_get', 'quest_get'])
+    )]
+    #[OA\Tag(name: 'players',description: "API Players")]
     #[Route('/{id}/quest', name: 'get_current_player_quest', methods: 'GET')]
     public function getCurrentPlayerQuest($id) {
         $player = $this->entityManager->getRepository(Player::class)->find($id);
@@ -50,6 +82,11 @@ class PlayerController extends AbstractApiController
         return $this->renderSerializeJson('Vous avez terminé toutes les quêtes disponible, n\'hesitez pas à faire réclamation aux près d\'Alexis qui n\'a pas assez rempli la BDD OMG WTF WHAT ARE YOU DOING');
     }
 
+    #[OA\Response(
+        response: 200,
+        description: "Retourne le score actuel du joueur",
+    )]
+    #[OA\Tag(name: 'players',description: "API Players")]
     #[Route('/{id}/score', name: 'get_current_player_score', methods: 'GET')]
     public function getCurrentPlayerScore($id) {
         $player = $this->entityManager->getRepository(Player::class)->find($id);
