faafa00042e0014699362acc8e6d2d1468fa6e90
Baptiste Magliocca <baptiste.mag@live.fr>
2024-05-26 17:52:31 +0200

    ✨ Doc api
    Ajout de la documentation api

--- back/src/Controller/PlayerQuestController.php
+++ back/src/Controller/PlayerQuestController.php
@@ -8,13 +8,30 @@ use App\Entity\PlayerQuest;
 use App\Service\PlayerQuestService;
 use App\Service\PlayerService;
 use App\Service\QuestService;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
 #[Route('/api/player_quests', name: 'api_player_quests_')]
 class PlayerQuestController extends AbstractApiController
 {
+    #[OA\Response(
+        response: 200,
+        description: "Retourne la nouvelle quête du joueur",
+        content: new Model(type: PlayerQuest::class, groups: ['player_quest_get', 'quest_get', 'player_get'])
+    )]
+    #[OA\RequestBody(
+        description: "Envoi l'id du joueur à qui valider sa quête en cours et lui en associer une nouvelle",
+        required: true,
+        content: [new OA\MediaType(mediaType: "multipart/form-data",
+            schema: new OA\Schema(
+                properties: [new OA\Property(property: "player", type: "int",description: "id du joueur")]
+            )
+        )]
+    )]
+    #[OA\Tag(name: 'player_quests',description: "API Player Quests")]
     #[Route('/valid', name: 'valid_player_quest', methods: 'POST')]
     public function validPlayerQuest(Request $request, PlayerQuestService $playerQuestService, QuestService $questService, PlayerService $playerService) {
         try {
@@ -32,6 +49,21 @@ class PlayerQuestController extends AbstractApiController
         }
     }
 
+    #[OA\Response(
+        response: 200,
+        description: "Retourne la premère quête du joueur",
+        content: new Model(type: PlayerQuest::class, groups: ['player_quest_get', 'quest_get', 'player_get'])
+    )]
+    #[OA\RequestBody(
+        description: "Envoi l'id du joueur à qui associer sa première quête",
+        required: true,
+        content: [new OA\MediaType(mediaType: "multipart/form-data",
+            schema: new OA\Schema(
+                properties: [new OA\Property(property: "player", type: "int",description: "id du joueur")]
+            )
+        )]
+    )]
+    #[OA\Tag(name: 'player_quests',description: "API Player Quests")]
     #[Route('', name: 'first_player_quest', methods: 'POST')]
     public function firstPlayerQuest(Request $request, PlayerQuestService $playerQuestService, QuestService $questService, PlayerService $playerService) {
         try {
